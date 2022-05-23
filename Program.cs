42b3173aff2e0075329f35fb5875302646eaab0f
AnnisaDmynt221 <97298883+AnnisaDmynt221@users.noreply.github.com>
2022-05-23 21:04:23 +0700

    UTS_Pemrograman_4260

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uts_pemro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            dosen inidosen = new dosen(1, "Kamarudin", 21114260, "Laki-laki", "Pemrograman");
+            inidosen.Hasil();
+            Console.WriteLine();
+            inidosen.addDosen();
+            inidosen.addCourse();
+            Console.WriteLine();
+            inidosen.HasilAkhir();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ dosen.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uts_pemro
+{
+    class dosen
+    {
+        private int id { get; set; }
+        private string nama { get; set; }
+        private int nik { get; set; }
+        private string gender { get; set; }
+        private string course { get; set; }
+
+        public dosen(int id, String nama, int nik, String gender, String course)
+        {
+            this.id = id;
+            this.nama = nama;
+            this.nik = nik;
+            this.gender = gender;
+            this.course = course;
+        }
+
+        public void Hasil()
+        {
+            Console.WriteLine("Data Dosen");
+            Console.WriteLine();
+            Console.WriteLine($"ID      : {this.id}");
+            Console.WriteLine($"Nama    : {this.nama}");
+            Console.WriteLine($"NIK     : {this.nik}");
+            Console.WriteLine($"Gender  : {this.gender}");
+            Console.WriteLine($"Course  : {this.course}");    
+        }
+
+        public void addDosen()
+        {
+            Console.WriteLine("Inputkan Nama Dosen  : ");
+            this.nama = Console.ReadLine();
+            Console.Write("Inputkan NIK Dosen       : ");
+            this.nik = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public string addCourse()
+        {
+            Console.Write($"Inputkan Course {this.nama} : ");
+            this.course = Console.ReadLine();
+
+            return this.nama;
+        }
+
+        public void HasilAkhir()
+        {
+            Console.WriteLine("Biodata Dosen Baru");
+            Console.WriteLine();
+           // Console.WriteLine($"ID      : {this.id}");
+            Console.WriteLine($"Nama    : {this.nama}");
+            Console.WriteLine($"NIK     : {this.nik}");
+           // Console.WriteLine($"Gender  : {this.gender}");
+            Console.WriteLine($"Course  : {this.course}");
+        }
+    }
+}
